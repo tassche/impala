eca2931aa662ae6a2c08bbaf682d8ac23f709911
Tijl Van Assche <tijlvanassche@gmail.com>
2014-07-20 22:50:11 +0200

    Silent failure for missing instance folder

--- impala/__init__.py
+++ impala/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py', silent=True)
 
 from impala import views
 

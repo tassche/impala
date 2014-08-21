1914e3396eeb69458001ad460643059d632e23cc
Tijl Van Assche <tijlvanassche@gmail.com>
2014-08-21 22:21:18 +0200

    Refactor status and currentsong polling

--- impala/static/js/currentsong.js
+++ impala/static/js/currentsong.js
@@ -64,10 +64,89 @@ function resize_playlist() {
 }
 
 
+function on_poll_status_success(status) {
+    $('#status-bitrate').text(status.bitrate);
+    var time = status.time.split(':');
+    var progress = time[0] / time[1] * 100;
+    $('.progress-bar').css('width', progress+'%')
+        .attr('aria-valuenow', time[0])
+        .attr('aria-valuemax', time[1]);
+    if (status.playlist != playlist) {
+        $.ajax({
+            url: $SCRIPT_ROOT + '/mpd/playlistinfo',
+            dataType: 'json',
+            success: function(playlistinfo) {
+                populate_playlist(playlistinfo);
+                playlist = status.playlist;
+            }
+        });
+    }
+}
+
+function on_poll_status_error() {
+    $('#status-bitrate').text('');
+}
+
+function poll_status() {
+    var timeout;
+    $.ajax({
+        url: $SCRIPT_ROOT + '/poller/status',
+        dataType: 'json',
+        success: function(status) {
+            on_poll_status_success(status);
+            timeout = 500;
+        },
+        error: function() {
+            on_poll_status_error();
+            timeout = 5000;
+        },
+        complete: function() {
+            setTimeout(poll_status, timeout);
+        }
+    });
+}
+
+function on_poll_currentsong_success(currentsong) {
+    $('#currentsong-artist').text(currentsong.artist);
+    $('#currentsong-title').text(currentsong.title);
+    $('#currentsong-album').text(currentsong.album);
+    $('#currentsong-date').text(currentsong.date);
+}
+
+function on_poll_currentsong_error() {
+    $('#currentsong-artist').text('');
+    $('#currentsong-title').text('');
+    $('#currentsong-album').text('');
+    $('#currentsong-date').text('');
+}
+
+function poll_currentsong() {
+    var timeout;
+    $.ajax({
+        url: $SCRIPT_ROOT + '/poller/currentsong',
+        dataType: 'json',
+        success: function(currentsong) {
+            on_poll_currentsong_success(currentsong);
+            timeout = 500;
+        },
+        error: function() {
+            on_poll_currentsong_error();
+            timeout = 5000;
+        },
+        complete: function() {
+            resize_playlist();
+            setTimeout(poll_currentsong, timeout);
+        }
+    });
+}
+
+
 $(document).ready(function() {
     bind_playback_controls();
     bind_clear_playlist();
 
+    poll_status();
+    poll_currentsong();
     (function get_currentsong_time() {
         $.ajax({
             url: $SCRIPT_ROOT + '/poller/pretty_currentsong_time',
@@ -81,55 +160,6 @@ $(document).ready(function() {
                 $('#time-elapsed').text('');
                 $('#time-total').text('');
                 setTimeout(get_currentsong_time, 5000);
-            }
-        });
-    })();
-    (function get_currentsong() {
-        $.ajax({
-            url: $SCRIPT_ROOT + '/poller/currentsong',
-            dataType: 'json',
-            success: function(currentsong) {
-                $('#currentsong-artist').text(currentsong.artist);
-                $('#currentsong-title').text(currentsong.title);
-                $('#currentsong-album').text(currentsong.album);
-                $('#currentsong-date').text(currentsong.date);
-                setTimeout(get_currentsong, 500);
-            },
-            error: function() {
-                $('#currentsong-artist').text('');
-                $('#currentsong-title').text('');
-                $('#currentsong-album').text('');
-                setTimeout(get_currentsong, 5000);
-            },
-            complete: resize_playlist
-        });
-    })();
-    (function get_status() {
-        $.ajax({
-            url: $SCRIPT_ROOT + '/poller/status',
-            dataType: 'json',
-            success: function(status) {
-                $('#status-bitrate').text(status.bitrate);
-                var time = status.time.split(':');
-                var progress = time[0] / time[1] * 100;
-                $('.progress-bar').css('width', progress+'%')
-                    .attr('aria-valuenow', time[0])
-                    .attr('aria-valuemax', time[1]);
-                if (status.playlist != playlist) {
-                    $.ajax({
-                        url: $SCRIPT_ROOT + '/mpd/playlistinfo',
-                        dataType: 'json',
-                        success: function(playlistinfo) {
-                            populate_playlist(playlistinfo);
-                            playlist = status.playlist;
-                        }
-                    });
-                }
-                setTimeout(get_status, 500);
-            },
-            error: function() {
-                $('#status-bitrate').text('');
-                setTimeout(get_status, 5000);
             }
         });
     })();

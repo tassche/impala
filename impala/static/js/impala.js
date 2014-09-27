82099aa2fc2f05ff3bfb0ba44e84295c0b007d3b
Tijl Van Assche <tijlvanassche@gmail.com>
2014-09-27 21:12:39 +0200

    Move layout related functions to layout.js

--- impala/static/js/impala.js
+++ impala/static/js/impala.js
@@ -1,7 +1,3 @@
-var page = window.location.pathname;
-var viewport; // viewport class (xs, sm, md or lg)
-
-
 var playback_options = {consume: 0, random: 0, repeat: 0, single: 0};
 var volume;
 var volume_off = 0; // allows to toggle mute
@@ -177,29 +173,6 @@ function poll() {
 }
 
 
-function resize_components() {
-    var height = $(window).height();
-    height -= 50; // body padding-top
-    height -= $('#currentsong-mini').outerHeight(true);
-    height -= $('#controls').outerHeight(true);
-    height -= $('#quicknav').outerHeight(true);
-
-    height -= $('#lib-breadcrumbs').outerHeight(true);
-
-    $('#content').css('height', height);
-    $('#content .content').css('height', height);
-
-    if (viewport == 'sm') {
-        var margin = 20;
-        $('#lib-albums').css('height', (height-margin)/2);
-        $('#lib-songs').css('height', (height-margin)/2);
-    }
-}
-
-function update_viewport_class() {
-    viewport = get_viewport_class();
-}
-
 function update_playback_options(mpd_status) {
     var options = ['consume', 'random', 'repeat', 'single'];
     $.each(options, function(i, option) {
@@ -230,12 +203,6 @@ $(document).ready(function() {
         var attr = $('#nav-about').attr('class');
         $('#nav-about').attr('class', attr + ' active');
     }
-    update_viewport_class();
-    resize_components();
-    $(window).resize(function() {
-        update_viewport_class();
-        resize_components();
-    });
 });
 
 
@@ -260,15 +227,3 @@ function seconds_to_str(seconds) {
     return str;
 }
 
-function get_viewport_class() {
-    var w = window.outerWidth;
-    if (w < 768) {
-        return 'xs';
-    } else if (w < 992) {
-        return 'sm';
-    } else if (w < 1200) {
-        return 'md';
-    } else {
-        return 'lg';
-    }
-}
---
+++ impala/static/js/layout.js
@@ -0,0 +1,50 @@
+var page = window.location.pathname;
+var viewport; // viewport class (xs, sm, md or lg)
+
+
+function resize_components() {
+    var height = $(window).height();
+    height -= 50; // body padding-top
+    height -= $('#currentsong-mini').outerHeight(true);
+    height -= $('#controls').outerHeight(true);
+    height -= $('#quicknav').outerHeight(true);
+
+    height -= $('#lib-breadcrumbs').outerHeight(true);
+
+    $('#content').css('height', height);
+    $('#content .content').css('height', height);
+
+    if (viewport == 'sm') {
+        var margin = 20;
+        $('#lib-albums').css('height', (height-margin)/2);
+        $('#lib-songs').css('height', (height-margin)/2);
+    }
+}
+
+function update_viewport_class() {
+    viewport = get_viewport_class();
+}
+
+
+$(document).ready(function() {
+    update_viewport_class();
+    resize_components();
+    $(window).resize(function() {
+        update_viewport_class();
+        resize_components();
+    });
+});
+
+
+function get_viewport_class() {
+    var w = window.outerWidth;
+    if (w < 768) {
+        return 'xs';
+    } else if (w < 992) {
+        return 'sm';
+    } else if (w < 1200) {
+        return 'md';
+    } else {
+        return 'lg';
+    }
+}

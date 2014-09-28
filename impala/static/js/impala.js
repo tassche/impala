1ff89b6e7e664900018e2a71d1008fd3355c34c9
Tijl Van Assche <tijlvanassche@gmail.com>
2014-09-28 12:49:32 +0200

    Move playback control functions to controls.js

--- impala/static/js/impala.js
+++ impala/static/js/impala.js
@@ -1,52 +1,11 @@
+// impala.js //
+// requires controls.js
+
+
 var page = window.location.pathname;
 
-var playback_options = {consume: 0, random: 0, repeat: 0, single: 0};
-var volume;
-var volume_off = 0; // allows to toggle mute
 var playlist; // playlist version
 var updating_db = false;
-
-
-function bind_playback_controls() {
-    var elements = [
-        ['#play', 'play'],
-        ['#pause', 'pause?1'],
-        ['#stop', 'stop'],
-        ['#previous', 'previous'],
-        ['#next', 'next']
-    ];
-    $.each(elements, function(i, element) {
-        $(element[0]).click(function(event) {
-            $.get($SCRIPT_ROOT + '/mpd/' + element[1]);
-        });
-    });
-}
-
-function bind_playback_options() {
-    var options = ['consume', 'random', 'repeat', 'single'];
-    $.each(options, function(i, option) {
-        $('#' + option).click(function(event) {
-            var state = (playback_options[option] > 0) ? 0 : 1;
-            $.get($SCRIPT_ROOT + '/mpd/' + option + '?' + state);
-        });
-    });
-}
-
-function bind_volume_controls() {
-    $('#volume-down').click(function(event) {
-        var new_volume = (volume < 5) ? 0 : volume-5;
-        $.get($SCRIPT_ROOT + '/mpd/setvol?' + new_volume);
-    });
-    $('#volume-up').click(function(event) {
-        var new_volume = (volume > 95) ? 100 : volume+5;
-        $.get($SCRIPT_ROOT + '/mpd/setvol?' + new_volume);
-    });
-    $('#volume-off').click(function(event) {
-        new_volume = (volume_off > 0) ? volume_off : 0;
-        volume_off = (volume_off > 0) ? 0 : volume;
-        $.get($SCRIPT_ROOT + '/mpd/setvol?' + new_volume);
-    });
-}
 
 
 function update_currentsong() {
@@ -160,27 +119,7 @@ function poll() {
 }
 
 
-function update_playback_options(mpd_status) {
-    var options = ['consume', 'random', 'repeat', 'single'];
-    $.each(options, function(i, option) {
-        playback_options[option] = parseInt(mpd_status[option]);
-        var attr = $('#' + option).attr('class');
-        if (playback_options[option] > 0) {
-            $('#' + option).attr('class', attr + ' btn-info');
-        } else {
-            var index = attr.indexOf(' btn-info');
-            if (index > -1) {
-                $('#' + option).attr('class', attr.substring(0, index));
-            }
-        }
-    });
-}
-
-
 $(document).ready(function() {
-    bind_playback_controls();
-    bind_playback_options();
-    bind_volume_controls();
     poll();
 });
 

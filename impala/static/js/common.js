5902ed04ac8899fbb4e919512bc1a450da7211c2
Tijl Van Assche <tijlvanassche@gmail.com>
2014-10-09 20:48:35 +0200

    Merge alerts.js into common.js

--- impala/static/js/common.js
+++ impala/static/js/common.js
@@ -21,6 +21,57 @@
 // common.js //
 
 
+/// ALERTS ///
+
+var alert_div = $('#alert');
+var alert_class = {
+    info:    'alert alert-info',
+    success: 'alert alert-success'
+};
+var alert_text = {
+    db_update_in_progress: 'Database update in progress...',
+    db_update_finished:    'Database update finished.',
+    added_to_playlist:     ' added to playlist.',
+    playlist_cleared:      'Playlist cleared.'
+};
+
+
+function alert_db_update_in_progress() {
+    alert_alert(alert_class.info, alert_text.db_update_in_progress);
+}
+
+function alert_db_update_finished() {
+    alert_alert(alert_class.success, alert_text.db_update_finished);
+}
+
+function alert_added_to_playlist(what) {
+    alert_alert(alert_class.success, what + alert_text.added_to_playlist);
+    hide_alert();
+}
+
+function alert_playlist_cleared() {
+    alert_alert(alert_class.success, alert_text.playlist_cleared);
+    hide_alert();
+}
+
+
+function alert_alert(css_class, text) {
+    alert_div.attr('class', css_class);
+    alert_div.text(text);
+    alert_div.show();
+    resize_components();
+}
+
+function hide_alert() {
+    setTimeout(function() {
+        alert_div.hide();
+        resize_components();
+    }, 3000);
+}
+
+
+/// NAVIGATION ///
+
 function style_active_navbar_element(element) {
     element.attr('class', element.attr('class') + ' active');
 }
@@ -37,6 +88,7 @@ function update_navigation() {
         style_active_quicknav_element(active_quicknav_element);
     }
 }
+
 
 
 $(document).ready(function() {

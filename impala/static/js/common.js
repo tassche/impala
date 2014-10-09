89ed2b92e36788c18bbc8b07bf1ffa9bcab82b6e
Tijl Van Assche <tijlvanassche@gmail.com>
2014-10-09 21:02:33 +0200

    Merge layout.js into common.js

--- impala/static/js/common.js
+++ impala/static/js/common.js
@@ -70,6 +70,49 @@ function hide_alert() {
 }
 
 
+/// LAYOUT ///
+
+var viewport; // viewport class (xs, sm, md or lg)
+
+
+function resize_components() {
+    var height = $(window).height();
+    height -= 50; // body padding-top
+    height -= $('#alert:visible').outerHeight(true);
+    height -= $('#currentsong-mini').outerHeight(true);
+    height -= $('#controls').outerHeight(true);
+    height -= $('#quicknav').outerHeight(true);
+
+    height -= $('#lib-breadcrumbs').outerHeight(true);
+
+    $('#content').css('height', height);
+    $('#content .content').css('height', height);
+
+    if (viewport == 'sm') {
+        var margin = 20;
+        $('#lib-albums').css('height', (height-margin)/2);
+        $('#lib-songs').css('height', (height-margin)/2);
+    }
+}
+
+function update_viewport_class() {
+    viewport = get_viewport_class();
+}
+
+function get_viewport_class() {
+    var w = window.outerWidth;
+    if (w < 768) {
+        return 'xs';
+    } else if (w < 992) {
+        return 'sm';
+    } else if (w < 1200) {
+        return 'md';
+    } else {
+        return 'lg';
+    }
+}
+
+
 /// NAVIGATION ///
 
 function bind_nav_playlist_commands() {
@@ -115,5 +158,12 @@ $(document).ready(function() {
     bind_nav_playlist_commands();
     bind_nav_database_commands();
     update_navigation();
+
+    update_viewport_class();
+    resize_components();
+    $(window).resize(function() {
+        update_viewport_class();
+        resize_components();
+    });
 });
 
